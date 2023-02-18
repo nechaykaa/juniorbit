e47a93d5c53b7ed06c21e15a13b80e058aab6cdb
nechaykaa <nechaev.martin@yandex.ru>
2023-02-18 15:28:35 +0500

    feat(Employees): Add new inputs

--- pages/hr/employees/new.tsx
+++ pages/hr/employees/new.tsx
@@ -3,6 +3,8 @@ import Input from '@/components/common/Input';
 import TagsPicker from '@/components/common/TagsPicker';
 import Title from '@components/common/Title';
 import SidebarLayout from '@layouts/SidebarLayout';
+import Select from '@/components/common/Select';
+import LinkIcon from '@assets/ic_link.svg';
 import { useFormik } from 'formik';
 
 import { useRouter } from 'next/router';
@@ -17,7 +19,11 @@ const NewEmployeePage = (): JSX.Element => {
 			fio: '',
 			job: '',
 			email: '',
+			tel: '',
 			tg: '',
+			mentor: '',
+			hr: '',
+			tasks: '',
 		},
 		onSubmit: () => undefined,
 	});
@@ -43,9 +49,62 @@ const NewEmployeePage = (): JSX.Element => {
 			<Input
 				className='mb-3'
 				onChange={formik.handleChange}
+				name='email'
+				// value={formik.values.email ? `@${formik.values.email}` : ''}
+				value={formik.values.email}
+				placeholder='Электронная почта' />
+			<Input
+				className='mb-3'
+				onChange={formik.handleChange}
+				name='tel'
+				value={formik.values.tel}
+				placeholder='Номер телефона' />
+			<Input
+				className='mb-3'
+				onChange={formik.handleChange}
 				name='tg'
 				value={formik.values.tg ? `@${formik.values.tg}` : ''}
-				placeholder='Должность сотрудника' />
+				placeholder='Ссылка на Telegram' />
+			<Select
+				className='mb-3'
+				options={[
+					{
+						label: 'Евгений Рогозин',
+						value: '',
+					},
+					{
+						label: 'Александр Маслоу',
+						value: '',
+					},
+					{
+						label: 'Зинаида Пушкевич',
+						value: '',
+					},
+				]} 
+				placeholder='Добавить наставника' />
+			<Select
+				className='mb-3'
+				options={[
+					{
+						label: 'Татьяна Авдеева',
+						value: '',
+					},
+					{
+						label: 'Максим Федотов',
+						value: '',
+					},
+					{
+						label: 'Нина Патрушева',
+						value: '',
+					},
+				]} 
+				placeholder='Добавить HR' />
+			<Input
+				className='mb-3'
+				onChange={formik.handleChange}
+				name='tasks'
+				value={formik.values.tasks}
+				placeholder='Задачи сотрудника' />
 			<h2 className='text-BoldBodyText_18 mt-8 mb-5'>
 				В каких проектах будет работать сотрудник?
 			</h2>
@@ -58,6 +117,23 @@ const NewEmployeePage = (): JSX.Element => {
 					return [...prev];
 				})}
 				placeholder='Название проекта' />
+			<h2 className='text-BoldBodyText_18 mt-8 mb-5'>
+				Уникальная ссылка
+			</h2>
+			<div className='flex text-center align-middle py-[23px] pl-[20px] pr-[425] bg-lightGrey rounded-2xl'>
+				<LinkIcon />
+				<p className='text-primary text-BodyText_14 mt-[3px] ml-3'>
+					https://rosmol.ru/XxXxXxXxXx
+				</p>
+			</div>
+			<Button
+				variant='stroke'
+				color='primary'
+				className='mt-4 w-full'
+			>
+				Сгенерировать уникальную ссылку
+			</Button>
+			
 			<Button variant='filled' color='primary' className='mt-4 w-full'>
 				Сохранить
 			</Button>
