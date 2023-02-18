17f7e781b502344bfaac201ed5d43a9f5c72aa51
nechaykaa <nechaev.martin@yandex.ru>
2023-02-18 18:07:42 +0500

    feat(Feedbacks): Add feedbacks

--- backend/src/tables/feedbacks.ts
+++ backend/src/tables/feedbacks.ts
@@ -10,6 +10,40 @@ interface IFeedback {
 	}
 }
 
-const FEEDBACK_TABLE: IFeedback[] = [];
+const FEEDBACK_TABLE: IFeedback[] = [
+	{
+		id: 0,
+		answers: {
+			mentorName: 'Денис Хромов',
+			firstWorkDay: '2023-02-18T12:37:54.837Z',
+			rate: 8,
+			youUnderstandWork: 'Да',
+			managerSetsTasks: 'Круто! Классно! Люблю Росмолодёжь',
+			areDifficulty: 'Нет, совсем не сложно'
+		}
+	},
+	{
+		id: 1,
+		answers: {
+			mentorName: 'Илья Огнев',
+			firstWorkDay: '2023-02-18T12:37:54.837Z',
+			rate: 10,
+			youUnderstandWork: 'Да, всё отлично',
+			managerSetsTasks: 'Задачи отлично понимаю, всегда таким хотел заниматься',
+			areDifficulty: 'Вообще легко'
+		}
+	},
+	{
+		id: 2,
+		answers: {
+			mentorName: 'Денис Хромов',
+			firstWorkDay: '2023-02-18T12:37:54.837Z',
+			rate: 5,
+			youUnderstandWork: 'Не сказал бы',
+			managerSetsTasks: 'Задачи не совсем понимаю',
+			areDifficulty: 'Да, трудновато'
+		}
+	},
+];
 
 export default FEEDBACK_TABLE;
