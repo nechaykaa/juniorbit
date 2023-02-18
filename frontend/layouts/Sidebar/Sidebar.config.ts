8bf2024fd7c1a37500ea2eca2f8dc2c82fda1d13
nechaykaa <nechaev.martin@yandex.ru>
2023-02-18 17:11:58 +0500

    ready admin page

--- frontend/layouts/Sidebar/Sidebar.config.ts
+++ frontend/layouts/Sidebar/Sidebar.config.ts
@@ -4,6 +4,8 @@ import Employees from '@assets/sidebarIcons/employees.svg';
 import Feedback from '@assets/sidebarIcons/feedback.svg';
 import Home from '@assets/sidebarIcons/home.svg';
 import Projects from '@assets/sidebarIcons/projects.svg';
+import Analytic from '@assets/sidebarIcons/analytic.svg';
+import HR from '@assets/sidebarIcons/hr.svg';
 
 export type sidebarItemColorTypes = 'fill' | 'stroke';
 
@@ -77,7 +79,30 @@ const SIDEBAR_ITEMS_HR: ISidebarItem[] = [
 	},
 ];
 
+const SIDEBAR_ITEMS_ADMIN: ISidebarItem[] = [
+	{
+		label: 'Аналитика',
+		href: '/hr',
+		icon: Analytic,
+		colorType: 'stroke',
+		validateEndsWith: true,
+	},
+	{
+		label: 'Все сотрудники',
+		href: '/admin/employees',
+		icon: Employees,
+		colorType: 'stroke',
+	},
+	{
+		label: 'HR',
+		href: '/admin/humanresources',
+		icon: HR,
+		colorType: 'fill',
+	},
+];
+
 export {
 	SIDEBAR_ITEMS,
 	SIDEBAR_ITEMS_HR,
+	SIDEBAR_ITEMS_ADMIN,
 };
---
+++ frontend/pages/admin/employees/index.tsx
@@ -0,0 +1,103 @@
+import SidebarLayout from '@layouts/SidebarLayout';
+import EmployeeCard from '@components/employees/EmployeeCard';
+import TextHorizontalMenu from '@components/common/TextHorizontalMenu';
+
+import menuItems from '@shared/consts/textHorizontalMenuItems';
+
+import Star from '@assets/ic_star.svg';
+
+const EmployeesPage = (): JSX.Element => {
+	return(
+		<SidebarLayout className='px-20 pt-14'>
+			<h1 className='font-bold text-4xl mb-[8px]'>
+				Все сотрудники
+			</h1>
+			<div>
+				<TextHorizontalMenu items={menuItems} />
+			</div>
+			<div className='grid grid-cols-[1fr_auto]'>
+				<div className='flex flex-col mt-[26px]'>
+					<div className='flex mb-[17px]'>
+						<EmployeeCard 
+							duration='Telegram • +7 (922) 129-78-06'
+							name='Александр Исаков'
+							description='Начальник отдела дизайна • РЕМЦ'
+							className='mr-[22px]'
+							Icon={Star} />
+						<EmployeeCard 
+							duration='Telegram • +7 (922) 129-78-06'
+							name='Мартин Нечаев'
+							description='Главный программист • РЕМЦ'
+							className='mr-[22px]'
+							Icon={Star} />
+					</div>
+					<div className='flex mb-[17px]'>
+						<EmployeeCard 
+							duration='Telegram • +7 (922) 129-78-06'
+							name='Дима Истомин'
+							description='Генеральный менеджер • РЕМЦ'
+							className='mr-[22px]'
+							Icon={Star} />
+						<EmployeeCard 
+							duration='Telegram • +7 (922) 129-78-06'
+							name='Никита Касинцев'
+							description='Главный менеджер • Роскультцентр'
+							className='mr-[22px]'
+							Icon={Star} />
+					</div>
+					<div className='flex mb-[17px]'>
+						<EmployeeCard 
+							duration='Telegram • +7 (922) 129-78-06'
+							name='Александр Лопахин'
+							description='Управляющий • Роскультцентр'
+							className='mr-[22px]'
+							Icon={Star} />
+						<EmployeeCard 
+							duration='Telegram • +7 (922) 129-78-06'
+							name='Наталья Петухова'
+							description='Программист • Роспатриот'
+							className='mr-[22px]' />
+					</div>
+					<div className='flex mb-[17px]'>
+						<EmployeeCard 
+							duration='Telegram • +7 (922) 129-78-06'
+							name='Василий Лопатин'
+							description='Управляющий • Роскультцентр'
+							className='mr-[22px]' />
+						<EmployeeCard 
+							duration='Telegram • +7 (922) 129-78-06'
+							name='Ирина Крючкова'
+							description='Управляющий • Роскультцентр'
+							className='mr-[22px]' />
+					</div>
+					<div className='flex mb-[17px]'>
+						<EmployeeCard 
+							duration='Telegram • +7 (922) 129-78-06'
+							name='Мирон Фёдоров'
+							description='Менеджер • Роскультцентр'
+							className='mr-[22px]' />
+						<EmployeeCard 
+							duration='Telegram • +7 (922) 129-78-06'
+							name='Алексей Петухов'
+							description='Технический специалист • Роспатриот'
+							className='mr-[22px]' />
+					</div>
+					<div className='flex mb-[17px]'>
+						<EmployeeCard 
+							duration='Telegram • +7 (922) 129-78-06'
+							name='Владимир Пупкин'
+							description='Администратор • Роскультцентр'
+							className='mr-[22px]' />
+						<EmployeeCard 
+							duration='Telegram • +7 (922) 129-78-06'
+							name='Василий Дудников'
+							description='Event-менеджер • РЕМЦ'
+							className='mr-[22px]' />
+					</div>
+				</div>
+			</div>
+		</SidebarLayout>
+	);
+};
+
+export default EmployeesPage;
